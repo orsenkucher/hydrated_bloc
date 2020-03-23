f48d1db1237512474d1486707c47e946dc6bde40
Arseniy Kucher <beeffallo@gmail.com>
2020-03-24 01:32:47 +0200

    wip: tests covering new storage

--- test/hydrated_bloc_storage_test.dart
+++ test/hydrated_bloc_storage_test.dart
@@ -23,6 +23,39 @@ void main() {
         hydratedStorage.clear();
       });
 
+// TODO HydratedFutureStorage test
+      group('heavy load', () {
+        test('writes heavily', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          var storage = HydratedFutureStorage(Directory.current);
+          final token = 'CounterBloc';
+          await hydratedStorage.write(token, "");
+          final tasks = Iterable.generate(250, (i) => i).map((i) async {
+            final record = Iterable.generate(
+              i,
+              (i) => Iterable.generate(i, (j) => 'Point($i,$j);').toList(),
+            ).toList();
+            final jsoned = json.encode(record);
+            await storage.write(token, jsoned); //TODO remove await
+            // hydratedStorage = await HydratedBlocStorage.getInstance();
+            // expect(await storage.read(token), jsoned);// THIS will definitely crash
+
+            dynamic written;
+            try {
+              final string = await storage.read(token);
+              final object = json.decode(string);
+              written = object;
+            } on dynamic catch (_) {
+              written = null;
+            } // At least json is not corrupted
+            expect(written, isNotNull);
+          });
+
+          await Future.wait(tasks);
+          storage.clear();
+        });
+      });
+
       group('read', () {
         test('returns null when file does not exist', () async {
           hydratedStorage = await HydratedBlocStorage.getInstance();
@@ -33,35 +66,88 @@ void main() {
         });
 
         test('returns correct value when file exists', () async {
-          final file = File('./.hydrated_bloc.json');
-          file.writeAsStringSync(json.encode({
-            "CounterBloc": {"value": 4}
-          }));
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync(json.encode({"value": 4}));
           hydratedStorage = await HydratedBlocStorage.getInstance();
-          expect(hydratedStorage.read('CounterBloc')['value'] as int, 4);
+          expect(hydratedStorage.read(token)['value'] as int, 4);
+        });
+
+        test('returns correct list when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          final list = ["C418", "`<v1'alpha>`"];
+          file.writeAsStringSync(json.encode(list));
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          expect(hydratedStorage.read(token), list);
+        });
+
+        test('returns correct number when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync(3.1415.toString());
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          expect(hydratedStorage.read(token) as double, 3.1415);
+        });
+
+        test('returns correct string when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync('"3.1415.toString()"');
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          expect(hydratedStorage.read(token) as String, "3.1415.toString()");
         });
 
         test(
             'returns null value when file exists but contains corrupt json and deletes the file',
             () async {
-          final file = File('./.hydrated_bloc.json');
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
           file.writeAsStringSync("invalid-json");
           hydratedStorage = await HydratedBlocStorage.getInstance();
-          expect(hydratedStorage.read('CounterBloc'), isNull);
+          expect(hydratedStorage.read(token), isNull);
           expect(file.existsSync(), false);
         });
       });
 
       group('write', () {
-        test('writes to file', () async {
+        test('writes string to file', () async {
           hydratedStorage = await HydratedBlocStorage.getInstance();
           await hydratedStorage.write('CounterBloc', json.encode({"value": 4}));
-
           expect(hydratedStorage.read('CounterBloc'), '{"value":4}');
+        });
+
+        test('writes map to file', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          await hydratedStorage.write('CounterBloc', const {"value": 12});
+          expect(hydratedStorage.read('CounterBloc'), const {"value": 12});
         });
       });
 
       group('clear', () {
+        test('clear empty storage', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          await hydratedStorage.clear();
+        });
+
+        test('delete existed but vanished case', () async {
+          final token = 'CounterBloc';
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          await hydratedStorage.write(token, const {"value": 12});
+          final file = await File('./.bloc.$token.json').delete();
+          await hydratedStorage.delete(token);
+          expect(file.existsSync(), false);
+        });
+
+        test('clear existed but vanished case', () async {
+          final token = 'CounterBloc';
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          await hydratedStorage.write(token, const {"value": 12});
+          final file = await File('./.bloc.$token.json').delete();
+          await hydratedStorage.clear();
+          expect(file.existsSync(), false);
+        });
+
         test('calls deletes file, clears storage, and resets instance',
             () async {
           hydratedStorage = await HydratedBlocStorage.getInstance();
@@ -70,12 +156,17 @@ void main() {
           expect(hydratedStorage.read('CounterBloc'), '{"value":4}');
           await hydratedStorage.clear();
           expect(hydratedStorage.read('CounterBloc'), isNull);
-          final file = File('./.hydrated_bloc.json');
+          final file = File('./.bloc.CounterBloc.json');
           expect(file.existsSync(), false);
         });
       });
 
       group('delete', () {
+        test('delete non existing token', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance();
+          await hydratedStorage.delete("Nothing");
+        });
+
         test('does nothing for non-existing key value pair', () async {
           hydratedStorage = await HydratedBlocStorage.getInstance();
 
@@ -96,6 +187,9 @@ void main() {
       });
     });
 
+// TODO(OMG) is it possible to have one set of tests,
+// and provide just different instances of `hydratedStorage`, bro)?
+// Because I have A LOT to test otherwise!!!!!!!
     group('Custom Storage Directory', () {
       HydratedBlocStorage hydratedStorage;
 
@@ -115,41 +209,108 @@ void main() {
         });
 
         test('returns correct value when file exists', () async {
-          final file = File('./.hydrated_bloc.json');
-          file.writeAsStringSync(json.encode({
-            "CounterBloc": {"value": 4}
-          }));
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync(json.encode({"value": 4}));
           hydratedStorage = await HydratedBlocStorage.getInstance(
             storageDirectory: Directory.current,
           );
-          expect(hydratedStorage.read('CounterBloc')['value'] as int, 4);
+          expect(hydratedStorage.read(token)['value'] as int, 4);
+        });
+
+        test('returns correct list when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          final list = ["C418", "`<v1'alpha>`"];
+          file.writeAsStringSync(json.encode(list));
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          expect(hydratedStorage.read(token), list);
+        });
+
+        test('returns correct number when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync(3.1415.toString());
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          expect(hydratedStorage.read(token) as double, 3.1415);
+        });
+
+        test('returns correct string when file exists', () async {
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
+          file.writeAsStringSync('"3.1415.toString()"');
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          expect(hydratedStorage.read(token) as String, "3.1415.toString()");
         });
 
         test(
             'returns null value when file exists but contains corrupt json and deletes the file',
             () async {
-          final file = File('./.hydrated_bloc.json');
+          final token = 'CounterBloc';
+          final file = File('./.bloc.$token.json');
           file.writeAsStringSync("invalid-json");
           hydratedStorage = await HydratedBlocStorage.getInstance(
             storageDirectory: Directory.current,
           );
-          expect(hydratedStorage.read('CounterBloc'), isNull);
+          expect(hydratedStorage.read(token), isNull);
           expect(file.existsSync(), false);
         });
       });
 
       group('write', () {
-        test('writes to file', () async {
+        test('writes string to file', () async {
           hydratedStorage = await HydratedBlocStorage.getInstance(
             storageDirectory: Directory.current,
           );
           await hydratedStorage.write('CounterBloc', json.encode({"value": 4}));
-
           expect(hydratedStorage.read('CounterBloc'), '{"value":4}');
+        });
+
+        test('writes map to file', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          await hydratedStorage.write('CounterBloc', const {"value": 12});
+          expect(hydratedStorage.read('CounterBloc'), const {"value": 12});
         });
       });
 
       group('clear', () {
+        test('clear empty storage', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          await hydratedStorage.clear();
+        });
+
+        test('delete existed but vanished case', () async {
+          final token = 'CounterBloc';
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          await hydratedStorage.write(token, const {"value": 12});
+          final file = await File('./.bloc.$token.json').delete();
+          await hydratedStorage.delete(token);
+          expect(file.existsSync(), false);
+        });
+
+        test('clear existed but vanished case', () async {
+          final token = 'CounterBloc';
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          await hydratedStorage.write(token, const {"value": 12});
+          final file = await File('./.bloc.$token.json').delete();
+          await hydratedStorage.clear();
+          expect(file.existsSync(), false);
+        });
+
         test('calls deletes file, clears storage, and resets instance',
             () async {
           hydratedStorage = await HydratedBlocStorage.getInstance(
@@ -160,12 +321,19 @@ void main() {
           expect(hydratedStorage.read('CounterBloc'), '{"value":4}');
           await hydratedStorage.clear();
           expect(hydratedStorage.read('CounterBloc'), isNull);
-          final file = File('./.hydrated_bloc.json');
+          final file = File('./.bloc.CounterBloc.json');
           expect(file.existsSync(), false);
         });
       });
 
       group('delete', () {
+        test('delete non existing token', () async {
+          hydratedStorage = await HydratedBlocStorage.getInstance(
+            storageDirectory: Directory.current,
+          );
+          await hydratedStorage.delete("Nothing");
+        });
+
         test('does nothing for non-existing key value pair', () async {
           hydratedStorage = await HydratedBlocStorage.getInstance(
             storageDirectory: Directory.current,
