78a2a1b0cf72f54f6ab9654b0cdaffb2f63caf01
Arseniy Kucher <orsen.kucher@gmail.com>
2020-07-14 20:05:05 +0300

    prepare for write traverse

--- lib/src/hydrated_bloc.dart
+++ lib/src/hydrated_bloc.dart
@@ -34,6 +34,7 @@ abstract class HydratedBloc<Event, State> extends Bloc<Event, State> {
     if (_state != null) return _state;
     try {
       final stateJson = storage.read(storageToken);
+      // json.encode(value)
       if (stateJson == null) return _state = super.state;
       // return _state = fromJson(Map<String, dynamic>.from(stateJson));
       return _state = fromJson(_traverse(stateJson));
@@ -43,25 +44,109 @@ abstract class HydratedBloc<Event, State> extends Bloc<Event, State> {
     }
   }
 
+  //   void writeObject(object) {
+  //   // Tries stringifying object directly. If it's not a simple value, List or
+  //   // Map, call toJson() to get a custom representation and try serializing
+  //   // that.
+  //   if (writeJsonValue(object)) return;
+  //   _checkCycle(object);
+  //   try {
+  //     var customJson = _toEncodable(object);
+  //     if (!writeJsonValue(customJson)) {
+  //       throw JsonUnsupportedObjectError(object, partialResult: _partialResult);
+  //     }
+  //     _removeSeen(object);
+  //   } catch (e) {
+  //     throw JsonUnsupportedObjectError(object,
+  //         cause: e, partialResult: _partialResult);
+  //   }
+  // }
+
+//   bool writeJsonValue(object) {
+//   if (object is num) {
+//     if (!object.isFinite) return false;
+//     writeNumber(object);
+//     return true;
+//   } else if (identical(object, true)) {
+//     writeString('true');
+//     return true;
+//   } else if (identical(object, false)) {
+//     writeString('false');
+//     return true;
+//   } else if (object == null) {
+//     writeString('null');
+//     return true;
+//   } else if (object is String) {
+//     writeString('"');
+//     writeStringContent(object);
+//     writeString('"');
+//     return true;
+//   } else if (object is List) {
+//     _checkCycle(object);
+//     writeList(object);
+//     _removeSeen(object);
+//     return true;
+//   } else if (object is Map) {
+//     _checkCycle(object);
+//     // writeMap can fail if keys are not all strings.
+//     var success = writeMap(object);
+//     _removeSeen(object);
+//     return success;
+//   } else {
+//     return false;
+//   }
+// }
+
+  // /// List of objects currently being traversed. Used to detect cycles.
+  // final List _seen = [];
+
+  // /// Check if an encountered object is already being traversed.
+  // ///
+  // /// Records the object if it isn't already seen. Should have a matching call to
+  // /// [_removeSeen] when the object is no longer being traversed.
+  // void _checkCycle(object) {
+  //   for (var i = 0; i < _seen.length; i++) {
+  //     if (identical(object, _seen[i])) {
+  //       throw JsonCyclicError(object);
+  //     }
+  //   }
+  //   _seen.add(object);
+  // }
+
+  // /// Remove [object] from the list of currently traversed objects.
+  // ///
+  // /// Should be called in the opposite order of the matching [_checkCycle]
+  // /// calls.
+  // void _removeSeen(object) {
+  //   assert(_seen.isNotEmpty);
+  //   assert(identical(_seen.last, object));
+  //   _seen.removeLast();
+  // }
+
+  // bool _check(object) {
+  //   if (object is num) {
+  //     if (!object.isFinite) return false;
+  //     return true;
+  //   } else if (identical(object, true)) {
+  //     return true;
+  //   } else if (identical(object, false)) {
+  //     return true;
+  //   } else if (object == null) {
+  //     return true;
+  //   } else if (object is String) {
+  //     return true;
+  //   } else if (object is List) {
+  //     return true;
+  //   } else if (object is Map) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
   dynamic _traverse(dynamic value) {
-    // json.encode();
-    // var keyValueList = List(map.length * 2);
-    // var i = 0;
-    // var allStringKeys = true;
-    // map.forEach((key, value) {
-    //   if (key is! String) {
-    //     allStringKeys = false;
-    //   }
-    //   keyValueList[i++] = key;
-    //   keyValueList[i++] = value;
-    // });
-    // if (!allStringKeys) return false;
-    // writeString('{');
-    // var separator = '"';
-    // for (var i = 0; i < keyValueList.length; i += 2) {
     if (value is Map) {
-      // final map = Map<String, dynamic>.from(value);
-      final map = <String, dynamic>{}; // TODO: not String key?
+      final map = <String, dynamic>{};
       value.forEach((key, value) {
         map[key] = _traverse(value);
       });
